b4d2f8d36b6cd97705225aa42f0d47a0a90c1954
helliio <liang@dhcp-042127.wlan.ntnu.no>
2015-02-18 13:56:44 +0100

    add rest test

---
+++ Rest/app/src/main/java/com/example/liang/rest/Greeting.java
@@ -0,0 +1,17 @@
+package com.example.liang.rest;
+
+/**
+ * Created by liang on 18/02/15.
+ */
+public class Greeting {
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
